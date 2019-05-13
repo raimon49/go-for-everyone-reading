6947cfcc3daf85b9c8585e2089dafcd2ff2275c2
raimon <raimon49@hotmail.com>
2019-05-14 00:45:11 +0900

    Learn in chapter 01
    * Use LDFLAGS
    * Go way style Makefile
    * Exclusively controlled Key-Value implementation

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # go-for-everyone-reading
+
+## Environment
+
+* Go 1.12.4
+* Vim 8.1 (1-1161)
---
+++ ch01/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var revision string
+var goversion string
+var builddate string
+
+type KeyValue struct {
+	store map[string]string
+	mu    sync.RWMutex
+}
+
+func NewKeyValue() *KeyValue {
+	return &KeyValue{store: make(map[string]string)}
+}
+
+func (kv *KeyValue) Set(key, val string) {
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	kv.store[key] = val
+}
+
+func (kv *KeyValue) Get(key string) (string, bool) {
+	kv.mu.RLock()
+	defer kv.mu.RUnlock()
+	val, ok := kv.store[key]
+	return val, ok
+}
+
+func main() {
+	kv := NewKeyValue()
+	kv.Set("key", "Hello, world")
+	value, ok := kv.Get("key")
+	if ok {
+		fmt.Println(value)
+		fmt.Println()
+		fmt.Println("revision:  " + revision)
+		fmt.Println("goversion: " + goversion)
+		fmt.Println("builddate: " + builddate)
+	}
+
+}
