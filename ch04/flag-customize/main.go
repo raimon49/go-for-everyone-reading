424b22f9717643379c5fb4a3598eabc66431c1b1
raimon <raimon49@hotmail.com>
2019-05-19 18:20:59 +0900

    Add a code of use 'flag' package in customize case

---
+++ ch04/flag-customize/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	flags := flag.NewFlagSet("example", flag.ContinueOnError)
+	// メッセージの出力先を標準エラーから標準出力に変更
+	flags.SetOutput(os.Stdout)
+	// ヘルプメッセージの変更
+	flags.Usage = func() {
+		fmt.Println("Usage: This is customized usage message")
+	}
+	if err := flags.Parse(os.Args[1:]); err != nil {
+		// flag.ExitOnErrorでなくflag.ContinueOnErrorで独自のエラー処理
+		fmt.Printf("Handle parse errors")
+	}
+}
