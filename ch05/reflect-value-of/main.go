84c47dc9f4afe56004499a3b095b414d10880c5e
raimon <raimon49@hotmail.com>
2019-09-25 21:21:06 +0900

    Add a code about raise panic patterns in reflection

--- ch05/reflect-value-of/main.go
+++ ch05/reflect-value-of/main.go
@@ -26,4 +26,14 @@ func main() {
 	fmt.Printf("xv = %d\n", xv.Int())
 	xv.SetInt(100)
 	fmt.Printf("xv (after SetInt()) = %d\n", xv.Int())
+
+	// ValueOf()で取り出したreflect.Valueは、それぞれの型に適合するメソッド以外を呼び出すとpanicを起こす
+	rv1 := reflect.ValueOf(1)
+	rv2 := reflect.ValueOf("Hello, World")
+	rv3 := reflect.ValueOf([]byte{0xa,0xd})
+	rv4 := reflect.ValueOf(make(chan string))
+	rv1.Int()        // .String()だとpanic
+	rv2.String()     // .Int()だとpanic
+	rv3.Slice(0, 0)  // .MapKeys()だとpanic
+	rv4.TrySend(rv2) // TrySend(rv1)だとpanic
 }
