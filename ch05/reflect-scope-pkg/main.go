572cea8b25af1d51738a2daf393a78cc4c397044
raimon <raimon49@hotmail.com>
2019-09-30 21:40:23 +0900

    Add a code of access to exported element

---
+++ ch05/reflect-scope-pkg/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type Person struct {
+	Name string
+	Age  int
+	p    int // 小文字で始まる要素はパッケージ外からreflectでアクセスできない
+}
+
+func main() {
+	p := Person{}
+	rt := reflect.TypeOf(p)
+	rv := reflect.ValueOf(p)
+
+	for i := 0; i < rv.NumField(); i++ {
+		ft := rt.Field(i)
+		fv := rv.Field(i)
+
+		if ft.PkgPath == "" {
+			fmt.Printf("ft(i) -> %#v\n", ft)
+			fmt.Printf("fv(i) -> %#v\n", fv.Interface())
+		}
+	}
+}
