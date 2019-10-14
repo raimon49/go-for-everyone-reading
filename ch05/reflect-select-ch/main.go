9506857c8633e05b77de2e25edda08703ba24081
raimon <raimon49@hotmail.com>
2019-10-14 18:22:36 +0900

    Activate commented out lines

--- ch05/reflect-select-ch/main.go
+++ ch05/reflect-select-ch/main.go
@@ -4,8 +4,8 @@ import (
 	"errors"
 	"fmt"
 	"os"
-	// "os/signal"
-	// "syscall"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -19,6 +19,10 @@ func _main() error {
 	if len(os.Args) < 2 {
 		return errors.New("prog [file1 file2 ...]")
 	}
+
+	// シグナル処理のおまじない
+	sigch := make(chan os.Signal, 1)
+	signal.Notify(sigch, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
 
 	return nil
 }
