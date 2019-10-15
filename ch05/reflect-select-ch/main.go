effdddde5956acbdaaaa9124fa9a99a5f9fcf95d
raimon <raimon49@hotmail.com>
2019-10-15 21:22:33 +0900

    Implement internal functions

--- ch05/reflect-select-ch/main.go
+++ ch05/reflect-select-ch/main.go
@@ -5,8 +5,34 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"reflect"
 	"syscall"
 )
+
+func makeChannelsForFiles(files []string) ([]reflect.Value, error) {
+	cs := make([]reflect.Value, len(files))
+
+	for i, fn := range files {
+		// データを流すためのチャンネルを作成
+		ch := make(chan []byte)
+
+		// ファイルをオープン
+		f, err := os.Open(fn)
+		if err != nil {
+			return nil, err
+		}
+
+		go readFromFile(ch, f)
+
+		cs[i] = reflect.ValueOf(ch)
+	}
+
+	return cs, nil
+}
+
+func readFromFile(ch chan []byte, f *os.File) {
+	// TODO
+}
 
 func main() {
 	if err := _main(); err != nil {
@@ -23,6 +49,13 @@ func _main() error {
 	// シグナル処理のおまじない
 	sigch := make(chan os.Signal, 1)
 	signal.Notify(sigch, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
+
+	// os.Argsの最初の引数はこのコマンド名で、2番目以降が対象ファイル名
+	// channels, err := makeChannelsForFiles(os.Args[1:])
+	_, err := makeChannelsForFiles(os.Args[1:])
+	if err != nil {
+		return nil
+	}
 
 	return nil
 }
